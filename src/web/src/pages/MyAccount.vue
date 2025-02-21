f00459966be6638ff228ded386d76b1e4b2b9fac
Troy <troy4560@gmail.com>
2025-02-21 14:12:28 -0500

    myaccount page setup

---
+++ src/web/src/pages/MyAccount.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="account-page">
+      <h1>My Account</h1>
+      <p>Name: {{ user.name }}</p>
+      <p>Email: {{ user.email }}</p>
+      <!-- more to come.. -->
+    </div>
+  </template>
+  
+  <script>
+  import { mapGetters } from "vuex";
+  
+  export default {
+    name: "MyAccount",
+    computed: {
+      ...mapGetters({
+        user: "CURRENT_USER_INFO",
+      }),
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .account-page {
+    max-width: 600px;
+    margin: auto;
+    padding: 20px;
+    text-align: center;
+  }
+  </style>
+  
