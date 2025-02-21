2c67e0481363d8878ccb4847ab6f764f076222a8
Troy <troy4560@gmail.com>
2025-02-21 14:23:04 -0500

    datafetch!

--- src/web/src/pages/MyAccount.vue
+++ src/web/src/pages/MyAccount.vue
@@ -1,21 +1,39 @@
 <template>
     <div class="account-page">
       <h1>My Account</h1>
-      <p>Name: {{ user.name }}</p>
-      <p>Email: {{ user.email }}</p>
-      <!-- more to come.. -->
+      <p v-if="user">Name: {{ user.name }}</p>
+      <p v-if="user">Email: {{ user.email }}</p>
+      <p v-else>Loading user information...</p> <!-- Debugging message -->
     </div>
   </template>
   
   <script>
-  import { mapGetters } from "vuex";
+  import { mapGetters, mapActions } from "vuex";
+  import { userTypes } from "../store/modules/user";
   
   export default {
     name: "MyAccount",
     computed: {
       ...mapGetters({
-        user: "CURRENT_USER_INFO",
+        user: userTypes.getters.CURRENT_USER_INFO,
+        isLoggedIn: userTypes.getters.IS_LOGGED_IN,
       }),
+    },
+    methods: {
+    ...mapActions([userTypes.actions.LOAD_SESSION_COOKIE]),  
+  },
+    async mounted() {
+      console.log("User in MyAccount.vue:", this.user); // Debugging
+  
+      // If user is not loaded, attempt to load it from the store
+      if (!this.user) {
+        console.log("Fetching user data...");
+        try {
+          await this.$store.dispatch(userTypes.actions.LOAD_SESSION_COOKIE);
+        } catch (err) {
+          console.error("Error loading user:", err);
+        }
+      }
     },
   };
   </script>
